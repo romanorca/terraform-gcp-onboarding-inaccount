5c997fca4f53dfd0860346d16fb392c237049d59
romanorca <92960561+romanorca@users.noreply.github.com>
2021-12-03 17:13:11 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,9 @@
 variable "target_project_id" {
-    description = "The gcp project id"
+    description = "The GCP Project ID that you want to Orca Security to scan"
     type = string
 }
 variable "service_project_id" {
-    description = "The Orca Service Project id"
+    description = "The Orca Service Project ID"
     type = string
 }
+
